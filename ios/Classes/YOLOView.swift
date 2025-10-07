9b3ddd13ecc03c86589332bbc5ac4424f115997c
kalyan-veeravalli5g6 <kalyan.veeravalli@beamx.co>
2025-10-07 17:48:52 +0530

    updated yolo view in iOS

--- ios/Classes/YOLOView.swift
+++ ios/Classes/YOLOView.swift
@@ -546,28 +546,28 @@ public class YOLOView: UIView, VideoCaptureDelegate {
           var bestClass = ""
 
           switch task {
-          // case .detect:
-          //   let prediction = predictions.boxes[i]
-          //   rect = CGRect(
-          //     x: prediction.xywhn.minX, y: 1 - prediction.xywhn.maxY, width: prediction.xywhn.width,
-          //     height: prediction.xywhn.height)
-          //   bestClass = prediction.cls
-          //   confidence = CGFloat(prediction.conf)
-          //   let colorIndex = prediction.index % ultralyticsColors.count
-          //   boxColor = ultralyticsColors[colorIndex]
-          //   label = String(format: "%@ %.1f", bestClass, confidence * 100)
-          //   alpha = CGFloat((confidence - 0.2) / (1.0 - 0.2) * 0.9)
-          //   return
-          // default:
-          //   let prediction = predictions.boxes[i]
-          //   let clsIndex = prediction.index
-          //   rect = prediction.xywhn
-          //   bestClass = prediction.cls
-          //   confidence = CGFloat(prediction.conf)
-          //   label = String(format: "%@ %.1f", bestClass, confidence * 100)
-          //   let colorIndex = prediction.index % ultralyticsColors.count
-          //   boxColor = ultralyticsColors[colorIndex]
-          //   alpha = CGFloat((confidence - 0.2) / (1.0 - 0.2) * 0.9)
+          case .detect:
+            // let prediction = predictions.boxes[i]
+            // rect = CGRect(
+            //   x: prediction.xywhn.minX, y: 1 - prediction.xywhn.maxY, width: prediction.xywhn.width,
+            //   height: prediction.xywhn.height)
+            // bestClass = prediction.cls
+            // confidence = CGFloat(prediction.conf)
+            // let colorIndex = prediction.index % ultralyticsColors.count
+            // boxColor = ultralyticsColors[colorIndex]
+            // label = String(format: "%@ %.1f", bestClass, confidence * 100)
+            // alpha = CGFloat((confidence - 0.2) / (1.0 - 0.2) * 0.9)
+            break
+           default:
+            let prediction = predictions.boxes[i]
+            let clsIndex = prediction.index
+            rect = prediction.xywhn
+            bestClass = prediction.cls
+            confidence = CGFloat(prediction.conf)
+            label = String(format: "%@ %.1f", bestClass, confidence * 100)
+            let colorIndex = prediction.index % ultralyticsColors.count
+            boxColor = ultralyticsColors[colorIndex]
+            alpha = CGFloat((confidence - 0.2) / (1.0 - 0.2) * 0.9)
           }
           var displayRect = rect
           switch UIDevice.current.orientation {
@@ -597,8 +597,8 @@ public class YOLOView: UIView, VideoCaptureDelegate {
           if ratio >= 1 {
             let offset = (1 - ratio) * (0.5 - displayRect.minX)
             if task == .detect {
-              let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: offset, y: -1)
-              displayRect = displayRect.applying(transform)
+              // let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: offset, y: -1)
+              // displayRect = displayRect.applying(transform)
             } else {
               let transform = CGAffineTransform(translationX: offset, y: 0)
               displayRect = displayRect.applying(transform)
@@ -606,10 +606,10 @@ public class YOLOView: UIView, VideoCaptureDelegate {
             displayRect.size.width *= ratio
           } else {
             if task == .detect {
-              let offset = (ratio - 1) * (0.5 - displayRect.maxY)
+              // let offset = (ratio - 1) * (0.5 - displayRect.maxY)
 
-              let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: offset - 1)
-              displayRect = displayRect.applying(transform)
+              // let transform = CGAffineTransform(scaleX: 1, y: -1).translatedBy(x: 0, y: offset - 1)
+              // displayRect = displayRect.applying(transform)
             } else {
               let offset = (ratio - 1) * (0.5 - displayRect.minY)
               let transform = CGAffineTransform(translationX: 0, y: offset)
@@ -618,10 +618,10 @@ public class YOLOView: UIView, VideoCaptureDelegate {
             ratio = (height / width) / (3.0 / 4.0)
             displayRect.size.height /= ratio
           }
-          displayRect = VNImageRectForNormalizedRect(displayRect, Int(width), Int(height))
+          // displayRect = VNImageRectForNormalizedRect(displayRect, Int(width), Int(height))
 
-          boundingBoxViews[i].show(
-            frame: displayRect, label: label, color: boxColor, alpha: alpha)
+          // boundingBoxViews[i].show(
+          //   frame: displayRect, label: label, color: boxColor, alpha: alpha)
 
         } else {
           boundingBoxViews[i].hide()
@@ -662,16 +662,17 @@ public class YOLOView: UIView, VideoCaptureDelegate {
 
           switch task {
           case .detect:
-            let prediction = predictions.boxes[i]
-            // For the detect task, invert y using "1 - maxY" as before
-            rect = CGRect(
-              x: prediction.xywhn.minX,
-              y: 1 - prediction.xywhn.maxY,
-              width: prediction.xywhn.width,
-              height: prediction.xywhn.height
-            )
-            bestClass = prediction.cls
-            confidence = CGFloat(prediction.conf)
+            // let prediction = predictions.boxes[i]
+            // // For the detect task, invert y using "1 - maxY" as before
+            // rect = CGRect(
+            //   x: prediction.xywhn.minX,
+            //   y: 1 - prediction.xywhn.maxY,
+            //   width: prediction.xywhn.width,
+            //   height: prediction.xywhn.height
+            // )
+            // bestClass = prediction.cls
+            // confidence = CGFloat(prediction.conf)
+            break
 
           default:
             let prediction = predictions.boxes[i]
@@ -699,12 +700,12 @@ public class YOLOView: UIView, VideoCaptureDelegate {
           rect.size.width *= videoCapture.longSide * scaleX
           rect.size.height *= videoCapture.shortSide * scaleY
 
-          boundingBoxViews[i].show(
-            frame: rect,
-            label: label,
-            color: boxColor,
-            alpha: alpha
-          )
+          // boundingBoxViews[i].show(
+          //   frame: rect,
+          //   label: label,
+          //   color: boxColor,
+          //   alpha: alpha
+          // )
         } else {
           boundingBoxViews[i].hide()
         }
